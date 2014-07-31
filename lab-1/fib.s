8392e7118f4683f97b9e249953e71e9332fb8b53
Date <tjjh89017@hotmail.com>
2014-07-31 21:03:12 +0800

    finish fibseq

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -15,19 +15,23 @@ fibonacci:
 
 	@ R4 = R0 - 0 (update flags)
 	@ if(R0 <= 0) goto .L3 (which returns 0)
-
+	subs r4, r0, #0
+	ble .L3
 	@ Compare R4 wtih 1
 	@ If R4 == 1 goto .L4 (which returns 1)
-
+	subs r0, r4, #1
+	beq .L4
 	@ R0 = R4 - 1
 	@ Recursive call to fibonacci with R4 - 1 as parameter
-
+	bl fibonacci
 	@ R5 = R0
 	@ R0 = R4 - 2
 	@ Recursive call to fibonacci with R4 - 2 as parameter
-
+	mov r5, r0
+	sub r0, r4, #2
+	bl fibonacci
 	@ R0 = R5 + R0 (update flags)
-
+	adds r0, r0, r5
 	pop {r3, r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION

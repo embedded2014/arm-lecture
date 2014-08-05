9eb7162510d3d2590ffc49b14a5cbae13ef2bf8c
paicc <tony233534@gmail.com>
2014-08-05 13:53:52 +0800

    Update fib.s

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -13,20 +13,22 @@ fibonacci:
 	@ PROLOG
 	push {r3, r4, r5, lr}
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
+	subs r4,r0,#0                   //R4 = R0 - 0 (update flags)
+	 
+	cmp r0 #0                       //if(R0 <= 0) goto .L3 (which returns 0)
+	ble .L3
+	
+	cmp r4,#1			// Compare R4 wtih 1
+	beq .L4				// If R4 == 1 goto .L4 (which returns 1)
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
+	subs r0,r4,#1 			// R0 = R4 - 1
+	bl fibonacci			// Recursive call to fibonacci with R4 - 1 as parameter
+	addi r5,r5,#0			// R5 = R0
+	
+	subs r0,r4,#2			// R0 = R4 - 2
+	bl fibonacci			// Recursive call to fibonacci with R4 - 2 as parameter
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
+	add r0,r5,r0			// R0 = R5 + R0 (update flags)
 
 	pop {r3, r4, r5, pc}		@EPILOG
 

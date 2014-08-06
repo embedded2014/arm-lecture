dfcb3363fe1c17dc3507e64dab0ce95f03782e7a
gn02596703 <tjes50220@gmail.com>
2014-08-06 20:44:16 +0800

    Update mutex.s

--- lab-2/mutex.s
+++ lab-2/mutex.s
@@ -9,7 +9,13 @@
 	.type lock_mutex, function
 lock_mutex:
         @ INSERT CODE BELOW
-
+	ldr r1, =locked
+.L1:
+	ldrex r2, [r0]
+	cmp r2, #0
+		strexeq r2, r1, [r0]
+		cmpeq r2, #0
+		bne .L1
         @ END CODE INSERT
 	bx lr
 
@@ -19,7 +25,8 @@ lock_mutex:
 	.type unlock_mutex, function
 unlock_mutex:
 	@ INSERT CODE BELOW
-        
+        ldr r1, =unlocked
+	str r1, [r0]
         @ END CODE INSERT
 	bx lr
 	.size unlock_mutex, .-unlock_mutex

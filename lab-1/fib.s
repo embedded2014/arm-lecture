8d26c1f460ff9610dbe4e697d7866c907b44d566
gn02596703 <tjes50220@gmail.com>
2014-08-12 21:54:11 +0800

    Update fib.s
    將 fibonacci 改成non-recursive
    把 r4當作counter
    每次執行 都將 r4 - 1
    若仍大於等於 -2 (跑到 -1 )就branch回去迴圈

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -9,40 +9,27 @@
 	.type fibonacci, function
 
 fibonacci:
-	@ ADD/MODIFY CODE BELOW
+@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, lr}
-
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
-	subs r4, r0, #0
-	ble .L3
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-	cmp r4, #1
-	beq .L4
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-	sub r0, r4, #1
-	bl fibonacci
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-	mov r5, r0
-	sub r0, r4, #2
-	bl fibonacci
-	@ R0 = R5 + R0 (update flags)
-	adds r0, r5, r0
-	pop {r3, r4, r5, pc}		@EPILOG
+	push {r3, r4, r5, r6, r7, lr}
+	
+	mov  r4, r0  @counter
+	mov  r5, #0
+	sub  r5, r5, #1   @previous
+	mov  r6, #0
+	add  r6, r6, #1   @result
+.L1:	
+	sub r4, r4, #1
+	add r7, r5, r6   @r7 = sum = previous + result
+	mov r5, r6
+	mov r6, r7
+	cmp r4 ,#-2
+		bgt .L1
+        mov r0, r5
+	pop {r3, r4, r5, r6, r7, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
 
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
